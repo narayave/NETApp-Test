86d424925d9a1ca9422749a4a05a9a383a83d339
Vedanth Narayanan <vedanth.nd@gmail.com>
2018-10-29 16:28:01 -0700

    Started a test .NET project

---
+++ NETApp/Models/PersonModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace NETApp.Models
+{
+    public class PersonModel
+    {
+		[Key]
+		[DatabaseGeneratedAttribute(DatabaseGeneratedOption.Identity)]
+		public int ID { get; set; }
+		public string Name { get; set; }
+		public int EventCount { get; set; }
+
+    }
+}
---
+++ NETApp/Models/RegisterEventModel.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NETApp.Models
+{
+    public class EventModel
+    {
+
+		[DatabaseGeneratedAttribute(DatabaseGeneratedOption.Identity)]
+		[Key]
+		public int ID { get; set; }
+		public string Name { get; set; }
+		public string Location { get; set; }
+		public DateTime DateTime { get; set; }
+		public string Details { get; set; }
+	}
+
+	public class RegisterEventModel
+	{
+		[Required]
+		[Display(Name = "ID")]
+		public string ID { get; set; }
+
+		[Required]
+		[Display(Name = "Name")]
+		public string Name { get; set; }
+
+		[Required]
+		[Display(Name = "Location")]
+		public string Location { get; set; }
+
+		[Required]
+		[Display(Name = "DateTime")]
+		public DateTime DateTime { get; set; }
+
+		[Display(Name = "Details")]
+		public string Details { get; set; }
+	}
+
+
+
+}
+
+
